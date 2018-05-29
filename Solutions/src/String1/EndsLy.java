a2fcb1d6c543e8006b75e27c9acff81e51197e0b
Saurabh Mithe <saurabhmithe@gmail.com>
2018-05-29 11:42:05 -0700

    String1 problems

---
+++ Solutions/src/String1/EndsLy.java
@@ -0,0 +1,18 @@
+package String1;
+
+public class EndsLy {
+
+    // Given a string, return true if it ends in "ly".
+
+    // endsLy("oddly") → true
+    // endsLy("y") → false
+    // endsLy("oddy") → false
+
+    public boolean endsLy(String str) {
+        if (str.length() > 1) {
+            return str.substring(str.length() - 2, str.length()).equals("ly");
+        }
+        return false;
+    }
+
+}
---
+++ Solutions/src/String1/WithoutEnd2.java
@@ -0,0 +1,16 @@
+package String1;
+
+public class WithoutEnd2 {
+
+    // Given a string, return a version without both the first and last char of the string.
+    // The string may be any length, including 0.
+
+    // withouEnd2("Hello") → "ell"
+    // withouEnd2("abc") → "b"
+    // withouEnd2("ab") → ""
+
+    public String withouEnd2(String str) {
+        return str.length() > 2 ? str.substring(1,str.length()-1) : "";
+    }
+
+}
