e23b078c8da67ef807fd0816f24e7241bd444258
Saurabh Mithe <saurabhmithe@gmail.com>
2018-05-29 11:22:58 -0700

    String1 problems

---
+++ Solutions/src/String1/FirstHalf.java
@@ -0,0 +1,15 @@
+package String1;
+
+public class FirstHalf {
+
+    // Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+    // firstHalf("WooHoo") → "Woo"
+    // firstHalf("HelloThere") → "Hello"
+    // firstHalf("abcdef") → "abc"
+
+    public String firstHalf(String str) {
+        return str.substring(0,str.length()/2);
+    }
+
+}
