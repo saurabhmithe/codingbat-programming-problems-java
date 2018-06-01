d9e095119b3cbaffea448578a98088165b9c01f5
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-01 01:07:45 -0700

    added Logic2 problems

---
+++ Solutions/src/Logic2/MakeBricks.java
@@ -0,0 +1,19 @@
+package Logic2;
+
+public class MakeBricks {
+
+    // We want to make a row of bricks that is goal inches long.
+    // We have a number of small bricks (1 inch each) and big bricks (5 inches each).
+    // Return true if it is possible to make the goal by choosing from the given bricks.
+    // This is a little harder than it looks and can be done without any loops.
+    // See also: Introduction to MakeBricks
+
+    // makeBricks(3, 1, 8) → true
+    // makeBricks(3, 1, 9) → false
+    // makeBricks(3, 2, 10) → true
+
+    public boolean makeBricks(int small, int big, int goal) {
+        return (goal % 5 > small) ? false : (small + big * 5) >= goal ? true : false;
+    }
+
+}
