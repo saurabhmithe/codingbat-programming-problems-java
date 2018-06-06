052f66f2b43ab57d75877d08e3bba94857444264
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-06 11:54:54 -0700

    added Recursion1 problems

---
+++ Solutions/src/Recursion1/BunnyEars.java
@@ -0,0 +1,19 @@
+package Recursion1;
+
+public class BunnyEars {
+
+    // We have a number of bunnies and each bunny has two big floppy ears.
+    // We want to compute the total number of ears across all the bunnies
+    // recursively (without loops or multiplication).
+
+    // bunnyEars(0) → 0
+    // bunnyEars(1) → 2
+    // bunnyEars(2) → 4
+
+    public int bunnyEars(int bunnies) {
+        if (bunnies == 0) return 0;
+        if (bunnies == 1) return 2;
+        return 2 + bunnyEars(bunnies - 1);
+    }
+
+}
