f6b33f803382b5652bed5454e1c6f4c93da282ba
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-06 12:05:43 -0700

    added Recursion1 problems

---
+++ Solutions/src/Recursion1/SumDigits.java
@@ -0,0 +1,19 @@
+package Recursion1;
+
+public class SumDigits {
+
+    // Given a non-negative int n, return the sum of its digits recursively (no loops).
+    // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+    // while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+    // sumDigits(126) → 9
+    // sumDigits(49) → 13
+    // sumDigits(12) → 3
+
+    public int sumDigits(int n) {
+        if (n == 0) return 0;
+        int digit = n % 10;
+        return digit + sumDigits(n / 10);
+    }
+
+}
