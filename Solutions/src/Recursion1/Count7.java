add6a7bb4c593d43b00b0b89edfd5f2973734749
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-06 12:17:36 -0700

    added Recursion1 problems

---
+++ Solutions/src/Recursion1/Count7.java
@@ -0,0 +1,20 @@
+package Recursion1;
+
+public class Count7 {
+
+    // Given a non-negative int n, return the count of the occurrences of 7 as a digit,
+    // so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost
+    // digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+    // count7(717) → 2
+    // count7(7) → 1
+    // count7(123) → 0
+
+    public int count7(int n) {
+        int count = 0;
+        if (n == 0) return 0;
+        if (n % 10 == 7) count = 1;
+        return count + count7(n / 10);
+    }
+
+}
