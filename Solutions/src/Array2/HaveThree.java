1f2ab119de5e752b169ff73d026ab7c83a0d8c80
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-09 01:58:02 -0700

    added Array2 problems

---
+++ Solutions/src/Array2/HaveThree.java
@@ -0,0 +1,26 @@
+package Array2;
+
+public class HaveThree {
+
+    // Given an array of ints, return true if the value 3 appears in the array exactly 3 times,
+    // and no 3's are next to each other.
+
+    // haveThree([3, 1, 3, 1, 3]) → true
+    // haveThree([3, 1, 3, 3]) → false
+    // haveThree([3, 4, 3, 3, 4]) → false
+
+    public boolean haveThree(int[] nums) {
+        int count = 0;
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] == 3) {
+                count += 1;
+                i += 2;
+            } else {
+                i += 1;
+            }
+        }
+        return count == 3;
+    }
+
+}
