4f124052af0ddad22db85db7ccee03a13676dadc
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-09 00:22:52 -0700

    added Array2 problems

---
+++ Solutions/src/Array2/CountEvens.java
@@ -0,0 +1,20 @@
+package Array2;
+
+public class CountEvens {
+
+    // Return the number of even ints in the given array.
+    // Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+    // countEvens([2, 1, 2, 3, 4]) → 3
+    // countEvens([2, 2, 0]) → 3
+    // countEvens([1, 3, 5]) → 0
+
+    public int countEvens(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) count += 1;
+        }
+        return count;
+    }
+
+}
