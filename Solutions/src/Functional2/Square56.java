b7e017d829dcb6061065f33d46bf6efed5638688
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-13 01:31:34 -0700

    completed Functional2 problems

---
+++ Solutions/src/Functional2/Square56.java
@@ -0,0 +1,20 @@
+package Functional2;
+
+import java.util.List;
+
+public class Square56 {
+
+    // Given a list of integers, return a list of those numbers squared and the product added to 10,
+    // omitting any of the resulting numbers that end in 5 or 6.
+
+    // square56([3, 1, 4]) → [19, 11]
+    // square56([1]) → [11]
+    // square56([2]) → [14]
+
+    public List<Integer> square56(List<Integer> nums) {
+        nums.replaceAll(n -> (n * n) + 10);
+        nums.removeIf(n -> n % 10 == 5 || n % 10 == 6);
+        return nums;
+    }
+
+}
