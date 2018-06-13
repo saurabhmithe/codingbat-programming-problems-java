0f67bb2bf0c06650cbfba9040cf5264ce4a5b48e
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-13 01:19:46 -0700

    added Functional2 problems

---
+++ Solutions/src/Functional2/NoNeg.java
@@ -0,0 +1,18 @@
+package Functional2;
+
+import java.util.List;
+
+public class NoNeg {
+
+    // Given a list of integers, return a list of the integers, omitting any that are less than 0.
+
+    // noNeg([1, -2]) → [1]
+    // noNeg([-3, -3, 3, 3]) → [3, 3]
+    // noNeg([-1, -1, -1]) → []
+
+    public List<Integer> noNeg(List<Integer> nums) {
+        nums.removeIf(n -> n < 0);
+        return nums;
+    }
+
+}
---
+++ Solutions/src/Functional2/NoTeen.java
@@ -0,0 +1,18 @@
+package Functional2;
+
+import java.util.List;
+
+public class NoTeen {
+
+    // Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive.
+
+    // noTeen([12, 13, 19, 20]) → [12, 20]
+    // noTeen([1, 14, 1]) → [1, 1]
+    // noTeen([15]) → []
+
+    public List<Integer> noTeen(List<Integer> nums) {
+        nums.removeIf(n -> n >= 13 && n <= 19);
+        return nums;
+    }
+
+}
