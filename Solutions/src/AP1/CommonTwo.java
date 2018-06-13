53b1dc779a09bed8ee810b2e983d9a93edb4944e
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-13 00:53:44 -0700

    completed AP1 problems

--- Solutions/src/AP1/CommonTwo.java
+++ Solutions/src/AP1/CommonTwo.java
@@ -10,4 +10,26 @@ public class CommonTwo {
     // commonTwo(["a", "c", "x"], ["b", "c", "d", "x"]) → 2
     // commonTwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) → 3
     // commonTwo(["a", "b", "c"], ["a", "b", "c"]) → 3
+
+    public int commonTwo(String[] a, String[] b) {
+        int count = 0;
+        int aPtr = 0;
+        int bPtr = 0;
+        String lastMatched = "";
+        while (aPtr < a.length && bPtr < b.length) {
+            if (a[aPtr].compareTo(b[bPtr]) == 0) {
+                if (!a[aPtr].equals(lastMatched)) {
+                    count += 1;
+                    lastMatched = a[aPtr];
+                }
+                aPtr += 1;
+                bPtr += 1;
+            } else {
+                if (a[aPtr].compareTo(b[bPtr]) < 0) aPtr += 1;
+                else bPtr += 1;
+            }
+        }
+        return count;
+    }
+
 }
